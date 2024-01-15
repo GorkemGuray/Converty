abf8e32dbd112ef738e85a7add2a0927a92fd460
Görkem Güray <grkmgry@gmail.com>
2024-01-15 21:59:57 +0300

    Initial Commit

---
+++ Converty/ConvertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertyApp.swift
+//  Converty
+//
+//  Created by Görkem Güray on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ConvertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
